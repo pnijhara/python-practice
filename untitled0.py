430f687dfc5130712b957e583198834ded64fada
Prajjwal Nijhara <prajjwalnijhara@gmail.com>
2018-06-11 00:17:56 +0530

    variables

--- untitled0.py
+++ untitled0.py
@@ -1,27 +1,14 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jun 10 11:56:05 2018
-
-@author: prime
-"""
-w = 'Wheresoever you go, go with all your heart'
-k = w.lower()
-l = k.split()
-s = ""
+string = 'Wheresoever you go, go with all your heart'   # or you can get an input from user
+lstring = string.lower()				# for converting string characters to lowercase
+l= lstring.split()					# spliting string on behalf of spaces, split() will convert string to list of string
+s = ''							# empty string 
 for i in range(0, len(l)):
     if  l[i].isalpha():
         s = s+l[i]
         if s[0]>"g":
-            print(s.capitalize())
+            print(s.upper())
             s = ""
         else:
             s = ""
     else:
         s = ""
-        
-        
-        
-        
-        
-        
