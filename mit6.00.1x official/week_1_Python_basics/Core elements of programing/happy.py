f24c0a4ae157141b9a4474b012589915653fc436
Prajjwal Nijhara <prajjwalnijhara@gmail.com>
2018-06-27 17:44:25 +0530

    updates

---
+++ mit6.00.1x official/week_1_Python_basics/Core elements of programing/happy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 27 17:37:18 2018
+
+@author: prime
+"""
+
+if happy > 2:
+    print("hello world")
---
+++ mit6.00.1x official/week_1_Python_basics/Core elements of programing/hello_world.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 27 17:33:14 2018
+
+@author: prime
+"""
+
+print("hello world")
