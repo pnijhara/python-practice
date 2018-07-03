cd1aea224fe06044cfd0372e5773346ceb8be80c
Prajjwal Nijhara <prajjwalnijhara@gmail.com>
2018-07-03 17:22:41 +0530

    update

---
+++ self/class_exercise.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  2 17:12:20 2018
+
+@author: prime
+"""
+
+class Weird(object):
+    def __init__(self, x, y): 
+        self.y = y
+        self.x = x
+    def getX(self):
+        return x 
+    def getY(self):
+        return y
+
+class Wild(object):
+    def __init__(self, x, y): 
+        self.y = y
+        self.x = x
+    def getX(self):
+        return self.x 
+    def getY(self):
+        return self.y
+
+X = 7
+Y = 8
